117c3cb12a45f43f11aaf94f1b0557c3faa5019e
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-29 16:15:41 -0700

    Create method to split key into a key subset of integers

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,13 @@ class Enigma
   def generate_key
     rand(0..99999).to_s.rjust(5, '0')
   end
+
+  def split_key
+    key = generate_key
+    key_subset = []
+    key.chars.each_cons(2) do |character|
+      key_subset << character.join.to_i
+    end
+    key_subset
+  end
 end
