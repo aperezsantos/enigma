33183827c8e772732714dc6d060700c35b3fb3d0
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-29 14:40:22 -0700

    Create test to generate string integer key using a stub

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,10 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_generates_key_as_a_string_integer
+    @enigma.stubs(:rand).returns(123)
+
+    assert_equal "00123", @enigma.generate_key
+  end
 end
