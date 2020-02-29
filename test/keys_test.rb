40cdfbb92be88f9340547667f39ada08f38e941e
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-28 21:39:25 -0700

    Move test to confirm length of random number as string from Keys to Number Generator

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -1,4 +1,5 @@
 require './test/test_helper'
+require './lib/number_generator'
 require './lib/keys'
 
 class KeysTest < Minitest::Test
@@ -8,17 +9,5 @@ class KeysTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Keys, @keys
-  end
-
-  def test_it_generates_random_number_as_string
-    @keys.generate_random_number
-
-    assert_equal String, @keys.generate_random_number.class
-  end
-
-  def test_random_number_generated_is_always_5_characters_long
-    @keys.generate_random_number
-
-    assert_equal 5, @keys.generate_random_number.length
   end
 end
--- test/number_generator_test.rb
+++ test/number_generator_test.rb
@@ -15,4 +15,13 @@ class NumberGeneratorTest < Minitest::Test
 
     assert_equal String, number_generator.generate_random_number.class
   end
+
+  def test_random_number_generated_is_always_5_characters_long
+    number_generator = NumberGenerator.new
+
+    number_generator.generate_random_number
+
+    assert_equal 5, number_generator.generate_random_number.length
+  end
+
 end
