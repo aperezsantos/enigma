b69d35dbf4fb4cd0e6a3c2612803d7a0c43ab77f
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-28 22:22:49 -0700

    Test Keys has the ability to save the generated random number

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -4,11 +4,16 @@ require './lib/keys'
 
 class KeysTest < Minitest::Test
   def setup
-    number_generator = NumberGenerator.new
-    @keys = Keys.new(number_generator)
+    @number_generator = NumberGenerator.new
+    @keys = Keys.new(@number_generator)
   end
 
   def test_it_exists
     assert_instance_of Keys, @keys
+  end
+
+  def test_it_has_way_to_save_random_number
+
+    assert_equal [], @keys.random_number
   end
 end
