55e80a88c7d3bf8cce27ad64882524b74efa1c49
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-28 22:28:13 -0700

    Include attr_reader and instance variable for number_generator argument

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,7 +1,8 @@
 class Keys
-  attr_reader :random_number
+  attr_reader :number_generator, :random_number
 
   def initialize(number_generator)
+    @number_generator = number_generator
     @random_number = []
   end
 end
