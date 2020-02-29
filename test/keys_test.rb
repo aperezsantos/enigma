31c60950b84b2fd80633256af9dd2a3e3d9e171d
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-28 20:11:07 -0700

    Test random number as string is always 5 characters long

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -15,4 +15,12 @@ class KeysTest < Minitest::Test
 
     assert_equal String, keys.generate_random_number.class
   end
+
+  def test_random_number_generated_is_always_5_characters_long
+    keys = Keys.new
+
+    keys.generate_random_number
+
+    assert_equal 5, keys.generate_random_number.length
+  end
 end
