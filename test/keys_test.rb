62bc99dcf1d6cd95d2fc4fe7ad2e662db282e22d
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-28 20:15:12 -0700

    Add setup to include instance of Keys

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -2,25 +2,23 @@ require './test/test_helper'
 require './lib/keys'
 
 class KeysTest < Minitest::Test
-  def test_it_exists
-    keys = Keys.new
+  def setup
+    @keys = Keys.new
+  end
 
-    assert_instance_of Keys, keys
+  def test_it_exists
+    assert_instance_of Keys, @keys
   end
 
   def test_it_generates_random_number_as_string
-    keys = Keys.new
+    @keys.generate_random_number
 
-    keys.generate_random_number
-
-    assert_equal String, keys.generate_random_number.class
+    assert_equal String, @keys.generate_random_number.class
   end
 
   def test_random_number_generated_is_always_5_characters_long
-    keys = Keys.new
+    @keys.generate_random_number
 
-    keys.generate_random_number
-
-    assert_equal 5, keys.generate_random_number.length
+    assert_equal 5, @keys.generate_random_number.length
   end
 end
