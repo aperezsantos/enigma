8a136a98bc5659b2cc5f4cf92cfe41660b496b1f
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-29 14:27:47 -0700

    Create setup and test firs test for Enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,13 +1,12 @@
 require './test/test_helper'
-require './lib/number_generator'
-require './lib/keys'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  def test_it_exists
-    enigma = Enigma.new
-
-    assert_instance_of Enigma, enigma
+  def setup
+    @enigma = Enigma.new
   end
 
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
 end
