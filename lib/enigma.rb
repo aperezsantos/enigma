8c4f30afea255c97124b3f419e67f8ab0ef020e9
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-02 14:58:00 -0700

    Refactor split_key method to pass in an argument

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,14 +9,14 @@ class Enigma
     rand(0..99999).to_s.rjust(5, '0')
   end
 
-  def split_key
-    key = generate_key
-    key_subset = []
-    key.chars.each_cons(2) do |character|
-      key_subset << character.join.to_i
+  def split_key(number)
+    key = []
+    number.chars.each_cons(2) do |character|
+      key << character.join.to_i
     end
-    key_subset
+    key
   end
+
 
   def generate_offset(date)
     last_four_digits = (date.to_i ** 2).to_s.slice(-4..-1)
@@ -33,4 +33,10 @@ class Enigma
     shifts[:d] = split_key[3] + generate_offset(date)[3]
     shifts
   end
+
+  def encrypt(message, key, date)
+   split_key
+   generate_offset
+   require "pry"; binding.pry
+ end
 end
